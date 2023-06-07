3cb7f00893f523ea6453d35cd3f9dbe242b68d0f
Franco <francoherrera_8@hotmail.com.ar>
2023-06-07 20:12:54 -0300

    app-routing form componente comented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './Componente/main/main.component';
 import { ContactoComponent } from './Paginas/contacto/contacto.component';
-import { FormularioComponent } from './Paginas/formulario/formulario.component';
 import { NosotrosComponent } from './Paginas/nosotros/nosotros.component';
 import { ProductosComponent } from './Paginas/productos/productos.component';
 
+/* import { FormularioComponent } from './Paginas/formulario/formulario.component'; */
 
 const routes: Routes = [
     { path: 'home', component: MainComponent },
     { path: 'nosotros', component: NosotrosComponent },
     { path: 'productos', component: ProductosComponent },
     { path: 'contacto', component: ContactoComponent },
-    { path: 'formulario', component: FormularioComponent },
+    /* { path: 'formulario', component: FormularioComponent }, */
     { path: '**', pathMatch: 'full', redirectTo: 'home'} 
 ];
 
