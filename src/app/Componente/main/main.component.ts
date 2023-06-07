df6417adb259d6772170324cfe506fc246339bf3
Franco <francoherrera_8@hotmail.com.ar>
2023-06-06 21:33:47 -0300

    main component con servicios de infoPagina Service equipo

--- src/app/Componente/main/main.component.ts
+++ src/app/Componente/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from 'src/app/Servicio/info-pagina.service';
 
 @Component({
   selector: 'app-main',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(public infoservicio: InfoPaginaService) { }
 
   ngOnInit(): void {
   }
