b93490455574d508f026235b6b9092f9be3c87cb
Franco <francoherrera_8@hotmail.com.ar>
2023-06-07 18:22:20 -0300

    productos component infoservicio add as public

--- src/app/Paginas/productos/productos.component.ts
+++ src/app/Paginas/productos/productos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from 'src/app/Servicio/info-pagina.service';
 
 @Component({
   selector: 'app-productos',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductosComponent implements OnInit {
 
-  constructor() { }
+  constructor( public infoservicio:InfoPaginaService) { }
 
   ngOnInit(): void {
   }
